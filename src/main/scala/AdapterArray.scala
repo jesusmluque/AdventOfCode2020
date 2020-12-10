1f05ed5325fe99f114c0e39487fae2c7084a3359
jesus <jesusmluque@gmail.com>
2020-12-10 20:02:02 +0100

    Adapter Array solution (day 10)

---
+++ src/main/scala/AdapterArray.scala
@@ -0,0 +1,53 @@
+object AdapterArray {
+  def calculateCombinations(adapters: List[Int]):Long = {
+    val adaptersOrdered = adapters.sorted
+    val first = traverseArray(adaptersOrdered, (List(0),0,0,0))
+
+    val res = first._1.reverse.tail.foldLeft((List[Long](0), List[Long]())) { (acc, n) =>
+      if (acc._1.reverse.head + 4 >= n)
+        (n :: acc._1, acc._2)
+      else if (acc._1.size == 5)
+        (List(n), 7 :: acc._2)
+      else if (acc._1.size == 4)
+        (List(n), 4 :: acc._2)
+      else if (acc._1.size == 3 && acc._1.max - acc._1.min <= 3)
+        (List(n), 2 :: acc._2)
+      else
+        (n :: acc._1.reverse.tail.reverse, acc._2)
+    }
+    val extra = if (res._1.size == 5)
+      7L :: res._2
+    else if (res._1.size == 4)
+      4L :: res._2
+    else if (res._1.size == 3 && res._1.max - res._1.min <= 3)
+      2L :: res._2
+    else
+      res._2
+
+    extra.product
+  }
+
+  def traverseArray(adapters: List[Int], acc: (List[Int],Int,Int,Int)): (List[Int],Int,Int,Int) = {
+    if (adapters.nonEmpty) {
+      val next = adapters.head
+      if (next == acc._1.head + 1)
+        traverseArray(adapters.tail, (next :: acc._1, acc._2 + 1, acc._3, acc._4))
+      else if (next == acc._1.head + 2)
+        traverseArray(adapters.tail, (next :: acc._1, acc._2, acc._3 + 1, acc._4))
+      else if (next == acc._1.head + 3)
+        traverseArray(adapters.tail, (next :: acc._1, acc._2, acc._3, acc._4 + 1))
+      else
+        acc
+    }
+    else
+      acc
+  }
+
+  def calculateJoltMultiplication(adapters: List[Int]) = {
+    val adaptersOrdered = adapters.sorted
+    val res = traverseArray(adaptersOrdered, (List(0),0,0,0))
+    System.out.println(res._1)
+    res._2 * (res._4 + 1)
+
+  }
+}
---
+++ src/test/scala/AdaptersArrayTest.scala
@@ -0,0 +1,30 @@
+import org.scalatest.FlatSpec
+
+import scala.io.Source
+
+class AdaptersArrayTest extends FlatSpec{
+
+  "The number of 1-jolt differences multiplied by the number of 3-jolt when the file is AdaptersExercise1.txt " should " be " in {
+    assert(AdapterArray.calculateJoltMultiplication(Source.fromResource("AdaptersExercise1.txt").getLines().toList.map(_.toInt)) == 35)
+  }
+
+  "The number of 1-jolt differences multiplied by the number of 3-jolt when the file is AdaptersExercise2.txt " should " be " in {
+    assert(AdapterArray.calculateJoltMultiplication(Source.fromResource("AdaptersExercise2.txt").getLines().toList.map(_.toInt)) == 220)
+  }
+
+  "The number of 1-jolt differences multiplied by the number of 3-jolt when the file is Adapters.txt " should " be " in {
+    assert(AdapterArray.calculateJoltMultiplication(Source.fromResource("Adapters.txt").getLines().toList.map(_.toInt)) == 1920)
+  }
+
+  "The total number of distinct ways you can arrange the adapters to connect the charging outlet to your device for the AdaptersExercise1.txt " should " be " in {
+    assert(AdapterArray.calculateCombinations(Source.fromResource("AdaptersExercise1.txt").getLines.toList.map(_.toInt)) == 8)
+  }
+
+  "The total number of distinct ways you can arrange the adapters to connect the charging outlet to your device for the AdaptersExercise2.txt " should " be " in {
+    assert(AdapterArray.calculateCombinations(Source.fromResource("AdaptersExercise2.txt").getLines.toList.map(_.toInt)) == 19208)
+  }
+
+  "The total number of distinct ways you can arrange the adapters to connect the charging outlet to your device for the Adapters.txt " should " be " in {
+    assert(AdapterArray.calculateCombinations(Source.fromResource("Adapters.txt").getLines.toList.map(_.toInt)) == 1511207993344L)
+  }
+}
